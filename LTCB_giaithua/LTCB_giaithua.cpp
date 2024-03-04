7357b5249b7f690ac8e37d34c9a448b8365f7fd7
ADMIN <ADMIN@LAB1-MAY33>
2024-03-04 11:13:23 +0700

    Add project files.

---
+++ LTCB_giaithua/LTCB_giaithua.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i = 1, sonhap, giaithua = 1;
+
+
+	cout << "Nhap so giai thua:";
+	cin >> sonhap;
+	while (i <= sonhap) {
+		giaithua = giaithua * i;
+		i++;
+
+	}
+	cout << "Giai thua cua " << sonhap << "la:" << giaithua << endl;
+
+	return 0;
+
+
+}
